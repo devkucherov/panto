36e2b107d6fd35b5a49c76c82817e57c14378963
Pasha Kucherov <dev.kucherov@gmail.com>
2023-12-26 15:29:17 +0300

    адаптировать секции products и footer, внести финальные правки

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 export const swiper = new Swiper('.swiper', {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 42,
   loop: true,
   freeMode: true,
@@ -12,18 +12,18 @@ export const swiper = new Swiper('.swiper', {
     prevEl: '#sliderPrev',
   },
 
-  // breakpoints: {
-  //   640: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20,
-  //   },
-  //   768: {
-  //     slidesPerView: 4,
-  //     spaceBetween: 40,
-  //   },
-  //   1024: {
-  //     slidesPerView: 5,
-  //     spaceBetween: 50,
-  //   },
-  // },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    920: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1230: {
+      slidesPerView: 4,
+      spaceBetween: 42,
+    },
+  },
 });
